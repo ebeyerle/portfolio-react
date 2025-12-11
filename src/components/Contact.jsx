ec703518f2f73881272f58211d28fceb3d6ac01f
Evan Beyerle <ebeyer2007@hotmail.com>
2025-12-11 12:50:58 -0500

    Update Contact.jsx
    Updated to current email address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/94014144-6d2d-4fc0-a03a-aa757b8070f6" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-[#000000]'>Contact</p>
-                <p className='text-[#000000] py-4'>Please fill out the contact form below or you can email me at: EBEYERL@g.clemson.edu</p>
+                <p className='text-[#000000] py-4'>Please fill out the contact form below or you can email me at: evanbeyerle@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
